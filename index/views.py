e8f6bea62d76f11dc4b3f1c62d4fe42be8d96d1b
Renan-Bodom <renan1211@hotmail.com>
2021-03-23 23:31:45 -0300

    Primeira template

--- index/views.py
+++ index/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Bem vindo sistema Web Plant Flow')
+    data = {}
+    data['sistema'] = "Web Plant Flow"
+    return render(request, 'index/index.html', data)
