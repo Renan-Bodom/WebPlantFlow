2a2effad1d2f100c1277a7cc12256101a3c18633
Renan-Bodom <renan1211@hotmail.com>
2021-04-06 21:44:33 -0300

    Configuração do app users

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.conta, name='url_conta'),
+    path('entrar/', views.login),
+    path('validar_acesso/', views.valida_senha),
+    path('sair/', views.sair)
+]
