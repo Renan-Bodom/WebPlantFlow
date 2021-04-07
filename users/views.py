be5563fea8fa2efbdd84ac273a77bcddc8f3abe6
Renan-Bodom <renan1211@hotmail.com>
2021-04-06 21:45:27 -0300

    Configuração sistema de login

--- users/views.py
+++ users/views.py
@@ -1,4 +1,48 @@
-from django.shortcuts import render
-from WebPlantFlow.decorators import validate_session, getSessionUser
+from django.shortcuts import render, redirect
+from django.contrib import messages
+import sys
+from WebPlantFlow.pyrebase_settings import db, auth
+from WebPlantFlow.decorators import clear_session
 
 # Create your views here.
+
+def conta(request):
+    return render(request,'users/conta.html')
+
+def login(request):
+    return render(request, 'users/login.html')
+
+
+def valida_senha(request):
+    email = request.POST.get('txtUserEmail', '')
+    senha = request.POST.get('txtUserPassword', '')
+    try:
+
+        sign_user = auth.sign_in_with_email_and_password(email, senha)
+        sign_user = auth.refresh(sign_user['refreshToken'])
+
+        userLab = db.child("users").child(sign_user['userId']).get()
+
+        request.session['idToken'] = sign_user['idToken']
+        request.session['userEmail'] = email
+        request.session['userId'] = sign_user['userId']
+        request.session['nomeUsuario'] = userLab.val()['nome']
+        request.session['perfilUsuario'] = userLab.val()['perfil']
+
+
+    except:
+        print("Entrou AQUI")
+        clear_session(request)
+        print(sys.exc_info()[1])
+        messages.error(request, "Usuário ou senha inválidos!")
+        return redirect('/usuario/entrar/')
+
+    return redirect('/')
+
+
+def sair (request):
+    # for key in request.session.keys():
+    # del request.session[key]
+    clear_session(request)
+
+    return render(request, "users/login.html")
