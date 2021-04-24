6d79868c86de7c6e0745f64bc76c4dfbbef485b1
Renan-Bodom <renan1211@hotmail.com>
2021-04-24 00:07:41 -0300

    Cada nova planta é salva dentro do subcaminho da espécie

--- myGarden/views.py
+++ myGarden/views.py
@@ -15,17 +15,22 @@ def myGarden(request):
     data['context']     = ""
 
     #########  Busca plantas já cadastradas pelo usuário
-    plantasSalvas = db.child(bancoJardim).child(request.session.get('userId')).get()
-    listaPlantas = []
+    plantasSalvas   = db.child(bancoJardim).child(request.session.get('userId')).get()
+    listaPlantas    = []
     try:
-        for plantaUser in plantasSalvas.each():
-            planta = plantaUser.val()
-            planta['popular'] = db.child('listaPlantas').child(planta['nomeCientifico']).get().val()['popular']
-            planta['foto'] = db.child('listaPlantas').child(planta['nomeCientifico']).get().val()['foto']
-            planta['info'] = db.child('listaPlantas').child(planta['nomeCientifico']).get().val()['informacoes']
-            listaPlantas.append(planta)
+        listaEspeciePlanta = []
+        for especiePlanta in plantasSalvas.each():
+            listaEspeciePlanta.append(especiePlanta.val())
+        for especieUser in listaEspeciePlanta:
+            for plantaUser in especieUser:
+                planta = especieUser[plantaUser]
+                planta['popular'] = db.child('listaPlantas').child(planta['nomeCientifico']).get().val()['popular']
+                planta['foto'] = db.child('listaPlantas').child(planta['nomeCientifico']).get().val()['foto']
+                planta['info'] = db.child('listaPlantas').child(planta['nomeCientifico']).get().val()['informacoes']
+                listaPlantas.append(planta)
     except:
         print('Jardim vazio')
+
     data['listaPlantas'] = listaPlantas
 
     return render(request, 'myGarden/myGarden.html', data)
@@ -51,20 +56,20 @@ def novaPlanta(request):
         formPronto = {"apelido":   formApelido,
                       "nomeCientifico":  formNomeCientifico,
                       "data": formData}
-        db.child(bancoJardim).child(request.session.get('userId')).child(formApelido).set(formPronto)
+        db.child(bancoJardim).child(request.session.get('userId')).child(formNomeCientifico).child(formApelido).set(formPronto)
 
         return redirect(urlJardim)
 
     return render(request, 'myGarden/novaPlanta.html', data)
 
 
-def excluirPlanta(request, planta):
-    db.child(bancoJardim).child(request.session.get('userId')).child(planta).remove()
+def excluirPlanta(request, especiePlantaSelc, plantaSelc):
+    db.child(bancoJardim).child(request.session.get('userId')).child(especiePlantaSelc).child(plantaSelc).remove()
 
     return redirect(urlJardim)
 
 
-def alterarPlanta(request, planta): # Falta carregar os dados para alterar no template
+def alterarPlanta(request, especiePlantaSelc,plantaSelc): # Falta carregar os dados para alterar no template
     data = {}
     data['SessionUser'] = getSessionUser(request)
     data['context']     = ""
@@ -73,28 +78,37 @@ def alterarPlanta(request, planta): # Falta carregar os dados para alterar no te
 
 
 
-def cuidadosPlanta(request, plantaSelc):
+def cuidadosPlanta(request, especiePlantaSelc, plantaSelc):
     data = {}
     data['SessionUser'] = getSessionUser(request)
     data['context'] = ""
 
     #########  Planta do usuário
-    plantaUser = db.child(bancoJardim).child(request.session.get('userId')).child(plantaSelc).get()
+    plantaUser = db.child(bancoJardim).child(request.session.get('userId')).child(especiePlantaSelc).child(plantaSelc).get()
     data['plantaUsuario'] = plantaUser.val()
 
+    #########  Planta de todos os usuarios
+    '''
+    plantaTodosUser = db.child(bancoJardim).child(*).child(plantaSelc).get()
+    data['plantaUsuario'] = plantaTodosUser.val()
+    for user in db.child(bancoJardim).get().each():
+        print(user.val())
+        '''
+
     #########  Planta do banco
-    plantaBanco = db.child('listaPlantas').child(plantaUser.val()['nomeCientifico']).get()
+    plantaBanco = db.child('listaPlantas').child(especiePlantaSelc).get()
     data['plantaBanco'] = plantaBanco.val()
 
+    # Formulario de cuidados
     if request.method == 'POST':
         formAgua = request.POST.get("agua", '')
         formSol = request.POST.get("sol", '')
 
         formPronto = {"agua": formAgua,
                       "sol": formSol}
-        db.child(bancoJardim).child(request.session.get('userId')).child(plantaSelc).child('cuidados').set(formPronto)
+        db.child(bancoJardim).child(request.session.get('userId')).child(especiePlantaSelc).child(plantaSelc).child('cuidados').set(formPronto)
 
 
-        return redirect('/meujardim/cuidadosPlanta/'+plantaSelc)
+        return redirect('/meujardim/cuidadosPlanta/'+especiePlantaSelc+'/'+plantaSelc)
 
     return render(request, 'myGarden/cuidadosPlanta.html', data)
