9c795c98aef26d168c1c607782e3befbbbb17b42
Renan-Bodom <renan1211@hotmail.com>
2021-04-27 11:57:41 -0300

    Função cuidados da comunidade

--- WebPlantFlow/funcoesTodos.py
+++ WebPlantFlow/funcoesTodos.py
@@ -1,10 +1,10 @@
 from WebPlantFlow.pyrebase_settings import db, auth
 
 #Calcula media e diferença com a comunidade
-def mediaDiferenca(qtdAgua, cuidadosPlantaTodos, plantaUser):
+def mediaDiferenca(qtdAgua, cuidadosPlantaTodos, plantaUser, itemCuidados):
     mediaQtdAgua = qtdAgua/len(cuidadosPlantaTodos)
     try:
-        diferenca = abs(mediaQtdAgua - int(plantaUser.val()['cuidados']['agua']))
+        diferenca = abs(mediaQtdAgua - int(plantaUser.val()['cuidados'][itemCuidados]))
     except:
         diferenca = abs(mediaQtdAgua - 0)
 
@@ -30,3 +30,23 @@ def plantasDoUsuario(plantasSalvas):
         print('Jardim vazio')
 
     return listaPlantas
+
+
+def cuidadosTodos (bancoJardim, especiePlantaSelc):
+    jardimTodosUser = db.child(bancoJardim).get()
+    plantasDaEspecie = []
+    for plantaTodos in jardimTodosUser.each():
+        try:
+            plantasDaEspecie.append(plantaTodos.val()[especiePlantaSelc])
+        except:
+            print('Jardineiro sem essa planta')
+
+    cuidadosPlantaTodos = []
+    for dadosPlantaTodos in plantasDaEspecie:
+        for nome in dadosPlantaTodos:
+            try:
+                cuidadosPlantaTodos.append(dadosPlantaTodos[nome]['cuidados'])
+            except:
+                print('Ninguém cuidando dessa planta')
+
+    return cuidadosPlantaTodos
