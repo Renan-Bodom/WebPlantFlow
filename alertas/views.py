3a0835ae03eff463d3e3309441813c2256ef917f
Renan-Bodom <renan1211@hotmail.com>
2021-04-27 18:26:32 -0300

    Buscando por alertas

--- alertas/views.py
+++ alertas/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from WebPlantFlow.decorators import validate_session, getSessionUser
 from WebPlantFlow.pyrebase_settings import db, auth
+from WebPlantFlow.funcoesTodos import mediaDiferenca, plantasDoUsuario, cuidadosTodos, mensagemAlerta
 
 # Bancos
 bancoJardim = 'myGarden'
@@ -14,39 +15,13 @@ def alertas(request):
 
     #########  Busca plantas já cadastradas pelo usuário
     plantasSalvas = db.child(bancoJardim).child(request.session.get('userId')).get()
-    listaPlantas = []
-    try:
-        listaEspeciePlanta = []
-        for especiePlanta in plantasSalvas.each():
-            listaEspeciePlanta.append(especiePlanta.val())
-        for especieUser in listaEspeciePlanta:
-            for plantaUser in especieUser:
-                planta = especieUser[plantaUser]
-                planta['popular'] = db.child('listaPlantas').child(planta['nomeCientifico']).get().val()['popular']
-                planta['foto'] = db.child('listaPlantas').child(planta['nomeCientifico']).get().val()['foto']
-                planta['info'] = db.child('listaPlantas').child(planta['nomeCientifico']).get().val()['informacoes']
-                listaPlantas.append(planta)
-    except:
-        print('Jardim vazio')
+    listaPlantas = plantasDoUsuario(plantasSalvas)
 
     mostrarAlertas = []
     for plantaUser in listaPlantas:
-        #########  Planta de todos os usuarios
+        #########  Encontrar alertas para as plantas
         try:
-            jardimTodosUser = db.child(bancoJardim).get()
-            plantasDaEspecie = []
-            for plantaTodos in jardimTodosUser.each():
-                try:
-                    plantasDaEspecie.append(plantaTodos.val()[plantaUser['nomeCientifico']])
-                except:
-                    print('Jardineiros sem esta planta')
-            cuidadosPlantaTodos = []
-            for dadosPlantaTodos in plantasDaEspecie:
-                for nome in dadosPlantaTodos:
-                    try:
-                        cuidadosPlantaTodos.append(dadosPlantaTodos[nome]['cuidados'])
-                    except:
-                        print('Ninguém cuidando dessa planta')
+            cuidadosPlantaTodos = cuidadosTodos(bancoJardim, plantaUser['nomeCientifico'], request.session.get('cidadeUsuario'))
 
             qtdAgua = 0
             qtdSol = 0
@@ -54,19 +29,17 @@ def alertas(request):
                 qtdAgua = qtdAgua + int(medir['agua'])
                 qtdSol = qtdSol + int(medir['sol'])
 
-            mediaQtdAgua = qtdAgua / len(cuidadosPlantaTodos)
-            data['mediaQtdAgua'] = mediaQtdAgua
-            if 30 < abs(mediaQtdAgua - int(plantaUser['cuidados']['agua'])):
-                mostrarAlertas.append('Faltando água na planta ' + plantaUser['popular'] + '('+ plantaUser['apelido'] +')')
-            else:
-                print('Água ok')
+            # Tentar reduzir custo computacional na linha de baixo!
+            plantaUser2 = db.child(bancoJardim).child(request.session.get('userId')).child(plantaUser['nomeCientifico']).child(plantaUser['apelido']).get()
 
-            mediaQtdSol = qtdSol / len(cuidadosPlantaTodos)
-            data['mediaQtdSol'] = mediaQtdSol
-            if 3 < abs(mediaQtdSol - int(plantaUser['cuidados']['sol'])):
-                mostrarAlertas.append('Faltando sol na planta ' + plantaUser['apelido'])
-            else:
-                print('Sol ok')
+            agua = mediaDiferenca(qtdAgua, cuidadosPlantaTodos, plantaUser2, 'agua')
+            if agua[1] > 20:
+                mostrarAlertas.append(mensagemAlerta('água', plantaUser))
+
+            sol = mediaDiferenca(qtdSol, cuidadosPlantaTodos, plantaUser2, 'sol')
+            if sol[1] > 1:
+                mostrarAlertas.append(mensagemAlerta('sol', plantaUser))
+
 
         except:
             print('Não foi possível comparar esta planta')
